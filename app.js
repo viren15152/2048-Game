f443079873ecd7f2f39dce20afb1870eba7ef297
Viren <144032304+viren15152@users.noreply.github.com>
2024-09-05 22:22:49 +0100

    Initialize DOM element selection for grid, score, and result displays

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const gridDisplay = document.querySelector('.grid')
+    const scoreDisplay = document.querySelector('score')
+    const resultDisplay = document.querySelector('result')
+    console.log(gridDisplay)
+
+    //create the playing board
+
+})
