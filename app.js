483267c39f877f898b8b1877b99df59619206edb
Viren <144032304+viren15152@users.noreply.github.com>
2024-09-05 22:37:28 +0100

    Added 16 divs to the grid for the numbers

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const scoreDisplay = document.querySelector('score')
     const resultDisplay = document.querySelector('result')
     console.log(gridDisplay)
+    const width = 4
 
     //create the playing board
+    function createBoard() {
+        for (let i = 0; i < width * width; i++) {
+            const sqaure = document.createElement('div')
+            gridDisplay.appendChild(sqaure)
+         
+        }
+    
+    }
+    createBoard()
 
 })
