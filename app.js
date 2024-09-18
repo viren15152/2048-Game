1ae4db15ed64a4c4e3513516e6ae22da4fe9c95b
Viren <144032304+viren15152@users.noreply.github.com>
2024-09-18 23:12:56 +0100

    Created function moveRight

--- app.js
+++ app.js
@@ -1,19 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
     const gridDisplay = document.querySelector('.grid')
-    const scoreDisplay = document.querySelector('score')
-    const resultDisplay = document.querySelector('result')
+    const scoreDisplay = document.querySelector('#score')
+    const resultDisplay = document.querySelector('#result')
     console.log(gridDisplay)
     const width = 4
-
+    let squares = []
+    
     //create the playing board
     function createBoard() {
         for (let i = 0; i < width * width; i++) {
-            const sqaure = document.createElement('div')
-            gridDisplay.appendChild(sqaure)
+            const square = document.createElement('div')
+            square.innerHTML = 0
+            gridDisplay.appendChild(square)
+            squares.push(square)
          
         }
-    
+        generate()                                
+        generate()
     }
     createBoard()
 
+    //generate a new number
+    function generate() {
+        const randomNumber = Math.floor(Math.random() * squares.length)
+        if (squares[randomNumber].innerHTML == 0) {
+            squares[randomNumber].innerHTML = 2
+            //checkForGameOver()
+        } else generate()
+    }
+
+    function moveRight() {
+        for (let i = 0; i < 16; i++) {
+            if (i % 4 === 0) {
+                let totalOne = squares[i].innerHTML
+                let totalTwo = squares[i+1].innerHTML
+                let totalThree = squares[i+2].innerHTML
+                let totalFour = squares[i+3].innerHTML
+                let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+                console.log(row)   
+
+                let filteredRow = row.filter(num => num)
+            }
+        }
+    }
+    moveRight()
+
 })
+
