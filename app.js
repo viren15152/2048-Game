f9b4d0c2bf7ddafe0e8eda2d9569c1979e01849b
Viren <144032304+viren15152@users.noreply.github.com>
2024-09-19 00:15:20 +0100

    added function moveLeft and combine row

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const gridDisplay = document.querySelector('.grid')
     const scoreDisplay = document.querySelector('#score')
     const resultDisplay = document.querySelector('#result')
-    console.log(gridDisplay)
     const width = 4
     let squares = []
+    let score = 0
     
     //create the playing board
     function createBoard() {
@@ -37,13 +37,83 @@ document.addEventListener('DOMContentLoaded', () => {
                 let totalThree = squares[i+2].innerHTML
                 let totalFour = squares[i+3].innerHTML
                 let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
-                console.log(row)   
 
                 let filteredRow = row.filter(num => num)
+                let missing = 4 - filteredRow.length
+                let zeros = Array(missing).fill(0)
+                let newRow = zeros.concat(filteredRow)
+
+                squares[i].innerHTML = newRow[0]
+                squares[i+1].innerHTML = newRow[1]
+                squares[i+2].innerHTML = newRow[2]
+                squares[i+3].innerHTML = newRow[3]
             }
         }
     }
-    moveRight()
 
+    function moveLeft() {
+        for (let i = 0; i < 16; i++) {
+            if (i % 4 === 0) {
+                let totalOne = squares[i].innerHTML
+                let totalTwo = squares[i+1].innerHTML
+                let totalThree = squares[i+2].innerHTML
+                let totalFour = squares[i+3].innerHTML
+                let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+                let filteredRow = row.filter(num => num)
+                let missing = 4 - filteredRow.length
+                let zeros = Array(missing).fill(0)
+                let newRow = filteredRow.concat(zeros)
+
+                squares[i].innerHTML = newRow[0]
+                squares[i+1].innerHTML = newRow[1]
+                squares[i+2].innerHTML = newRow[2]
+                squares[i+3].innerHTML = newRow[3]
+
+            }
+        }
+    }
+
+    function  combineRow() {
+        for (let i = 0; i < 15; i++) {
+            if (squares[i].innerHTML === squares[i+1].innerHTML) {
+                let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i+1].innerHTML)
+                squares[i].innerHTML = combinedTotal
+                squares[i+1].innerHTML = 0
+                score += combinedTotal
+                scoreDisplay.innerHTML = score
+
+            }
+
+        }
+        //checkForWin()
+    }
+
+    //assign function to keys 
+    function control(e) {
+        if(e.key === 'ArrowLeft') {
+            keyLeft()
+        } else if (e.key === 'ArrowRight') {
+            keyRight()
+        }
+    }
+    document.addEventListener('keydown', control)
+
+    function keyLeft() {
+        moveLeft()
+        combineRow()
+        moveLeft()
+        generate()
+    }
+
+    function keyRight() {
+        moveRight()
+        combineRow()
+        moveRight()
+        generate()
+    }
 })
+
+
+
 
