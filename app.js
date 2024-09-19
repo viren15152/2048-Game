74552eea27a7ca37db346a8e0e7f231536a74421
Viren <144032304+viren15152@users.noreply.github.com>
2024-09-19 21:49:29 +0100

    Added moveUp function to shift tiles vertically

--- app.js
+++ app.js
@@ -74,6 +74,26 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    function moveUp() {
+        for (let i = 0; i < 4; i++) {
+            let totalOne = squares[i].innerHTML
+            let totalTwo = squares[i+width].innerHTML
+            let totalThree = squares[i+width*2].innerHTML
+            let totalFour = squares[i+width*3].innerHTML
+            let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+            let filteredColumn = column.filter(num => num)
+            let missing = 4 - filteredColumn.length
+            let zeros = Array(missing).fill(0)
+            let newColumn = filteredColumn.concat(zeros)
+
+            squares[i].innerHTML = newColumn[0]
+            squares[i+width].innerHTML = newColumn[1]
+            squares[i+width*2].innerHTML = newColumn[2]
+            squares[i+width*3].innerHTML = newColumn[3] 
+        }
+    }
+
     function combineRow() {
         for (let i = 0; i < 15; i++) {
             if (squares[i].innerHTML === squares[i+1].innerHTML) {
@@ -98,7 +118,7 @@ document.addEventListener('DOMContentLoaded', () => {
         } else if (e.key === 'ArrowUp') {
             keyUp()
         } else if (e.key === 'ArrowDown') {
-            keyDown()
+            //keyDown()
         }
     }
     document.addEventListener('keydown', control)
@@ -119,7 +139,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function keyUp() {
         moveUp()
-        conmbineColumn()
+        //conmbineColumn()
         moveUp()
         generate()
     }
